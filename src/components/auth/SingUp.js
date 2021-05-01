48511cf7f78c02fa044f604d2802b5e5d8d6e89b
maamutio <miguel.amutioesteban@gmail.com>
2021-05-01 12:47:08 +0200

    Constructor, isSeller

--- src/components/auth/SingUp.js
+++ src/components/auth/SingUp.js
@@ -1,33 +1,55 @@
 import React from "react";
-import {Component} from "react";
-import {connect} from "react-redux";
-import {singUp} from "../../store/actions/authActions"
+import { Component } from "react";
+import { connect } from "react-redux";
+import { singUp } from "../../store/actions/authActions"
+import ToggleButton from 'react-toggle-button'
 
+export class SingUp extends Component {
 
-export class SingUp extends Component  {
-    state={
-        email:"",
-        password:"",
-        nombre:"",
-        apellidos:"",
-        isSeller: true
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {
+                email: "",
+                password: "",
+                nombre: "",
+                apellidos: "",
+                isSeller: true
+            }
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
 
-    handleChange= (e) => {
+    handleChange = (e) => {
         this.setState({
-            [e.target.id]: e.target.value
+            user:{
+                ...this.state.user,
+                [e.target.id]: e.target.value    
+            }})
+    }
+    handleSeller = (e) => {
+        this.setState({
+            user: {
+                ...this.state.user,
+                isSeller: !this.state.user.isSeller,
+
+            }
         })
     }
 
-    handleSubmit= (e) => {
+    handleSubmit = (e) => {
         e.preventDefault();
         console.log(this.state);
-        this.props.singUp(this.state);
+        this.props.singUp(this.state.user);
     }
 
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="container">
 
                 <form onSubmit={this.handleSubmit} className="white">
@@ -35,27 +57,27 @@ export class SingUp extends Component  {
                     <h5 className="grey-text text-darken-3">Registrarse</h5>
                     <div className="input-field">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" onChange={this.handleChange}/>
+                        <input type="email" id="email" onChange={this.handleChange} />
                     </div>
 
                     <div className="input-field">
                         <label htmlFor="password">Contraseña</label>
-                        <input type="password" id="password" onChange={this.handleChange}/>
+                        <input type="password" id="password" onChange={this.handleChange} />
                     </div>
-
+                    <p>{console.log(this.state)}</p>
                     <div className="input-field">
                         <label htmlFor="nombre">Nombre</label>
-                        <input type="text" id="nombre" onChange={this.handleChange}/>
+                        <input type="text" id="nombre" onChange={this.handleChange} />
                     </div>
 
                     <div className="input-field">
                         <label htmlFor="apellidos">Apellidos</label>
-                        <input type="text" id="apellidos" onChange={this.handleChange}/>
+                        <input type="text" id="apellidos" onChange={this.handleChange} />
                     </div>
 
                     <div className="input-field">
                         <label htmlFor="isSeller">isSeller</label>
-                        <input type="boolean" id="isSeller" onChange={this.handleChange}/>
+                        <ToggleButton className="form-control" name="isSeller" id="isSeller" value={this.state.user.isSeller || false} onToggle={this.handleSeller} />
                     </div>
 
                     <div className="input-field">
@@ -71,16 +93,16 @@ export class SingUp extends Component  {
     }
 }
 
-const mapStateToProps = (state) =>{
+const mapStateToProps = (state) => {
     return {
         auth: state.firebase.auth
     }
 }
 
-const mapDispatchToProps = (dispatch) =>{
-    return{
+const mapDispatchToProps = (dispatch) => {
+    return {
         singUp: (newUser) => dispatch(singUp(newUser))
     }
-    
+
 }
-export default connect(mapStateToProps, mapDispatchToProps) (SingUp)
+export default connect(mapStateToProps, mapDispatchToProps)(SingUp)
--- src/components/products/CreateProducts.js
+++ src/components/products/CreateProducts.js
@@ -1,44 +1,67 @@
 import React from "react";
-import {Component} from "react";
+import { Component } from "react";
 
 import { connect } from "react-redux"
-import { createProduct} from "../../store/actions/productActions"
+import { createProduct } from "../../store/actions/productActions"
+import {ToggleButton} from 'react-toggle-button';
+
+import { Redirect, withRouter } from 'react-router-dom';
 
 
-export class CreateProduct extends Component  {
-    state={
-        productName: "",
-        price:"",
-        description:"",
-        tienda:"",
-        id:"",
-        vendorId:""
-        
-    }
+export class CreateProduct extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            producto: {
+                productName: "",
+                price: "",
+                description: "",
+                tienda: "",
+                id: "",
+                vendorId: ""
+            },
+             page : {
+                redirect: false
+            } 
     
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    handleChange= (e) => {
+    /*    handleChange= (e) => {
         this.setState({
             [e.target.id]: e.target.value
         })
-    }
+    } */
+    handleChange = (e) => {
+        this.setState({
+            producto:{
+                ...this.state.producto,
+                [e.target.id] : e.target.value
+            }               
+        });
+    } 
 
-    handleSubmit=(e) => {
+
+
+    handleSubmit = (e) => {
         e.preventDefault();
         console.log(this.state)
         //console.log(user);
         this.setState({
             ...this.state
         })
-        this.props.createProduct(this.state);
-        
+        this.props.createProduct(this.state.producto);
     }
 
 
-    render(){
+    render() {
         /* const {user} = this.props;
         console.log(user); */
-        return(
+        return (
             <div className="container">
 
                 <form onSubmit={this.handleSubmit} className="white">
@@ -46,28 +69,29 @@ export class CreateProduct extends Component  {
                     <h5 className="grey-text text-darken-3">Añadir producto a su tienda</h5>
                     <div className="input-field">
                         <label htmlFor="productName">Nombre del Producto</label>
-                        <input type="text" id="productName" onChange={this.handleChange}/>
+                        <input type="text" id="productName" onChange={this.handleChange} />
                     </div>
 
                     <div className="input-field">
                         <label htmlFor="price">Precio (€)</label>
-                        <input type="text" id="price" onChange={this.handleChange}/>
+                        <input type="text" id="price" onChange={this.handleChange} />
                     </div>
+                    <p>{console.log(this.state)}</p>
 
                     <div className="input-field">
                         <label htmlFor="descripcion">Descripcion</label>
-                        <input type="text" id="description" onChange={this.handleChange}/>
+                        <input type="text" id="description" onChange={this.handleChange} />
                     </div>
 
-                    <div className="input-field"> 
+                    <div className="input-field">
                         <label htmlFor="tienda">Tienda</label>
-                        <input type="text" id="tienda" onChange={this.handleChange}/>
+                        <input type="text" id="tienda" onChange={this.handleChange} />
                     </div>
 
-                    <div className="input-field"> 
+                    <div className="input-field">
                         <label htmlFor="id">Id</label>
-                        <input type="id" id="id" onChange={this.handleChange}/>
-                    </div>                   
+                        <input type="id" id="id" onChange={this.handleChange} />
+                    </div>
 
                     <div className="input-field">
                         <button className="btn pink">Añadir</button>
